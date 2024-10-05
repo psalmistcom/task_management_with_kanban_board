b28f1bcffbb9014a39eb3afd6e46be87f7da45fb
psalmistcom <dadewale9@gmail.com>
2024-10-05 10:02:55 +0100

    dashboard content and backend done

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Resources\TaskResource;
+use App\Models\Task;
 use Illuminate\Http\Request;
 
 class DashboardController extends Controller
@@ -11,7 +13,49 @@ class DashboardController extends Controller
      */
     public function index()
     {
-        //
+        $user = auth()->user();
+
+        $totalPendingTask = Task::query()
+            ->where('status', 'pending')
+            ->count();
+
+        $myPendingPendingTask = Task::query()
+            ->where('status', 'pending')
+            ->where('assigned_user_id', $user->id)
+            ->count();
+
+        $totalProgressTasks = Task::query()
+            ->where('status', 'in_progress')
+            ->count();
+        $myProgressTasks = Task::query()
+            ->where('status', 'in_progress')
+            ->where('assigned_user_id', $user->id)
+            ->count();
+
+        $totalCompletedTasks = Task::query()
+            ->where('status', 'completed')
+            ->count();
+        $myCompletedTasks = Task::query()
+            ->where('status', 'completed')
+            ->where('assigned_user_id', $user->id)
+            ->count();
+
+        $activeTasks = Task::query()
+            ->whereIn('status', ['pending', 'in_progress'])
+            ->where('assigned_user_id', $user->id)
+            ->limit(10)
+            ->get();
+        $activeTasks = TaskResource::collection($activeTasks);
+
+        return inertia('Dashboard', compact(
+            'totalPendingTask',
+            'myPendingPendingTask',
+            'totalProgressTasks',
+            'myProgressTasks',
+            'totalCompletedTasks',
+            'myCompletedTasks',
+            'activeTasks'
+        ));
     }
 
     /**
